951de6a9f3733c991e925c5aec36f1b0f568edb2
Ben Merlotti <benmerlotti@bens-laptop.lan>
2024-08-01 11:16:08 -0400

    Final Commit

--- scripts/fishList.js
+++ scripts/fishList.js
@@ -2,8 +2,6 @@ import { database } from './fishData.js';
 
 // let fishArray = database.fish
 
-let fishInfo = document.getElementById("fishList")
-
 export const fishList = () => {
     // Generate an HTML representation of each fish
     database.fish.map(fish => {
@@ -19,3 +17,73 @@ export const fishList = () => {
 </article>`
     })
 };
+
+export const mostHolyFish = () => {
+    // 3, 6, 9, 12, etc... fish
+    let holyFish = ""
+
+    for (let fish of database.fish) {
+        if (fish.length % 3 === 0) {
+            holyFish +=
+`<article class="fishes">
+<img src="${fish.image}" class="fish__image">
+<div class="fish__details">
+<h1 class="fish__name">${fish.name}</h1>
+<p class="fish__species">Species: ${fish.species}</p>
+<p class="fish__diet">Diet: ${fish.diet}</p>
+<p class="fish__length">Length: ${fish.length} inches</p>
+</div>
+</article>`
+
+        }
+    }
+
+    return holyFish
+}
+
+export const soldierFish = () => {
+    // 5, 10, 15, 20, 25, etc... fish
+    let soldierFishes = ""
+    
+    for (let fish of database.fish) {
+        if (fish.length % 5 === 0) {
+            soldierFishes +=
+`<article class="fishes">
+<img src="${fish.image}" class="fish__image">
+<div class="fish__details">
+<h1 class="fish__name">${fish.name}</h1>
+<p class="fish__species">Species: ${fish.species}</p>
+<p class="fish__diet">Diet: ${fish.diet}</p>
+<p class="fish__length">Length: ${fish.length} inches</p>
+</div>
+</article>`
+
+        }
+    }
+
+    return soldierFishes
+}
+
+export const regularFish = () => {
+    // Any fish not a multiple of 3 or 5
+    let regFish = ""
+
+    for (let fish of database.fish) {
+        if (fish.length % 5 != 0 && fish.length % 3 != 0) {
+            regFish +=
+`<article class="fishes">
+<img src="${fish.image}" class="fish__image">
+<div class="fish__details">
+<h1 class="fish__name">${fish.name}</h1>
+<p class="fish__species">Species: ${fish.species}</p>
+<p class="fish__diet">Diet: ${fish.diet}</p>
+<p class="fish__length">Length: ${fish.length} inches</p>
+</div>
+</article>`
+
+        }
+    }
+
+    return regFish
+
+}
--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,16 @@
-import { fishList } from './fishList.js'
+import { fishList, mostHolyFish, regularFish, soldierFish } from './fishList.js'
 import { tipList } from './tipList.js'
 import { locationList } from './locationList.js'
 
+let fishInfo = document.getElementById("fishList")
+
 // Generate the fish list
-const fishHTML = fishList()
+// const fishHTML = fishList()
+const holyFishes = mostHolyFish()
+const soldierFishies = soldierFish()
+const regFishes = regularFish()
+
+fishInfo.innerHTML = `${holyFishes}${soldierFishies}${regFishes}`
 
 // Generate the care tips
 const tipHTML = tipList()
